01c7d11c4ea22284d571035ac287143e78c85254
prihodulkin <prihodulkin@mail.ru>
2021-10-13 14:25:41 +0300

    position detection added

--- Program.cs
+++ Program.cs
@@ -16,18 +16,9 @@ namespace AffineTransforms
         [STAThread]
         static void Main()
         {
-            var a = new Point(2, 2);
-            var line = new Point[2] { new Point(1, 1), new Point(3, 3) };
-            var result = Polygons.GetRelativePosition(a, line);
-           
-
-            a = new Point(5, 3);
-            line = new Point[2] { new Point(1, 1), new Point(3, 3) };
-            result = Polygons.GetRelativePosition(a, line);
-            var r = result;
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
