e1880d475e91e5c578d200a3d3e445b7114b8712
prihodulkin <prihodulkin@mail.ru>
2021-10-13 16:57:38 +0300

    polygons algorithms created

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace AffineTransforms
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
